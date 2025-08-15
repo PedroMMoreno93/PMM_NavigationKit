bbced09e4b0a0fea06c9c6b7ec0d553321f136ae
Pedro M Moreno <morenogonzalezpedro93@gmail.com>
2025-08-15 09:27:38 +0200

    (feature): coordinator implementation
    Including methods to handle navigation

--- PMM_NavigationKit/Sources/PMM_NavigationKit/Coordinator.swift
+++ PMM_NavigationKit/Sources/PMM_NavigationKit/Coordinator.swift
@@ -1,8 +1,51 @@
 //
-//  File.swift
+//  Coordinator.swift
 //  PMM_NavigationKit
 //
-//  Created by Pedro M Moreno on 15/8/25.
+//  Created by Pedro M Moreno.
 //
 
-import Foundation
+import SwiftUI
+
+@MainActor
+public final class Coordinator: ObservableObject {
+    @Published public var path: NavigationPath = .init()
+    @Published public var sheetPath: NavigationPath = .init()
+    @Published public var fullCoverPath: NavigationPath = .init()
+
+    public var sheetRoute: AnyHashable?
+    public var fullCoverRoute: AnyHashable?
+
+    public init() {}
+
+    public func push<R: Hashable>(_ route: R) {
+        path.append(route)
+    }
+
+    public func presentSheet<R: Hashable>(_ route: R) {
+        sheetRoute = route
+        sheetPath = NavigationPath()
+        sheetPath.append(route)
+    }
+
+    public func presentFullCover<R: Hashable>(_ route: R) {
+        fullCoverRoute = route
+        fullCoverPath = NavigationPath()
+        fullCoverPath.append(route)
+    }
+
+    public func pop() {
+        if !path.isEmpty { path.removeLast() }
+    }
+
+    public func dismissSheet() {
+        sheetPath = NavigationPath()
+        sheetRoute = nil
+    }
+
+    public func dismissFullCover() {
+        fullCoverPath = NavigationPath()
+        fullCoverRoute = nil
+    }
+}
+
